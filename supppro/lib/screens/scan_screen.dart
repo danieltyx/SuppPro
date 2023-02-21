9203fa0988ccfaf78a0029a928a14c795ed194d5
Yuxuan Tian <danieltian.yx@gmail.com>
2023-02-21 18:21:23 -0500

    1

--- supppro/lib/screens/scan_screen.dart
+++ supppro/lib/screens/scan_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class scanScreen extends StatefulWidget {
   const scanScreen({super.key});
@@ -36,6 +38,18 @@ class _scanScreenState extends State<scanScreen> {
     });
   }
 
+  Future<String> fetchProductName(String barcode) async {
+    final response = await http.get(Uri.parse(
+        'https://api.thesupplementdatabase.com/v1/products/barcode/$barcode'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['product'][0]['product_name'];
+    } else {
+      throw Exception('Failed to fetch product');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,10 +58,17 @@ class _scanScreenState extends State<scanScreen> {
         children: [
           Container(
             child: IconButton(
-              icon: Icon(Icons.camera),
-              onPressed: () => scanBarcodeNormal(),
-            ),
+                icon: Icon(Icons.camera),
+                onPressed: () {
+                  scanBarcodeNormal();
+                }),
           ),
+          IconButton(
+              onPressed: () {
+                if (_scanBarcode != 'Unknown' && _scanBarcode != -1)
+                  fetchProductName(_scanBarcode).then((value) => print(value));
+              },
+              icon: Icon(Icons.search)),
           Text('Scan result : $_scanBarcode\n', style: TextStyle(fontSize: 20))
         ],
       ),
