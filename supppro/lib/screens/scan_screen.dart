bcb965a1519797060c04053725d13c8c89bec49c
Yuxuan Tian <danieltian.yx@gmail.com>
2023-02-24 11:16:43 -0500

    Update scan_screen.dart

--- supppro/lib/screens/scan_screen.dart
+++ supppro/lib/screens/scan_screen.dart
@@ -16,10 +16,8 @@ class scanScreen extends StatefulWidget {
 class _scanScreenState extends State<scanScreen> {
   String _scanBarcode = 'Unknown';
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
           '#ff6666', 'Cancel', true, ScanMode.BARCODE);
@@ -28,9 +26,6 @@ class _scanScreenState extends State<scanScreen> {
       barcodeScanRes = 'Failed to get platform version.';
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
