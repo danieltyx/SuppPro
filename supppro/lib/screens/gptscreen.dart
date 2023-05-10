1045967b9b5f6f017643b02aba6d9abd14dda423
Yuxuan Tian <danieltian.yx@gmail.com>
2023-05-09 23:09:25 -0400

    Revert "Revert "tts component""
    This reverts commit 9faba9c5cdc15c5122feb7aa4af164dcc2e672dc.

--- supppro/lib/screens/gptscreen.dart
+++ supppro/lib/screens/gptscreen.dart
@@ -12,6 +12,7 @@ import 'package:supppro/providers/app_state.dart';
 import 'package:supppro/providers/suppItem.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class gptScreen extends StatefulWidget {
   const gptScreen({Key? key}) : super(key: key);
@@ -21,10 +22,13 @@ class gptScreen extends StatefulWidget {
 }
 
 class _gptScreenState extends State<gptScreen> {
+  final FlutterTts tts = FlutterTts();
+  String tts_text = "To begin, please click the Check button.";
+
   late OpenAI openAI;
   ChatCTResponse? mResponse;
   bool _isloadnig = false;
-  String response = "To begin, please click the \"Start\" button.";
+  String response = "To begin, please click the \"Check\" button.";
   String prompt = "empty";
   String promptQuestion = "empty";
   @override
@@ -36,6 +40,8 @@ class _gptScreenState extends State<gptScreen> {
         token: gpt_api_key,
         baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 30)),
         isLog: true);
+    tts.setLanguage('en');
+    tts.setSpeechRate(0.4);
     super.initState();
   }
 
@@ -75,7 +81,7 @@ class _gptScreenState extends State<gptScreen> {
       // print("${mResponse?.toJson()}");
       // print("@@@@@ $response");
     });
-
+    tts_text = response;
     return response;
   }
 
@@ -157,7 +163,12 @@ class _gptScreenState extends State<gptScreen> {
                       color: Colors.blue,
                       decoration: TextDecoration.underline),
                 ),
-              )
+              ),
+              TextButton(
+                  onPressed: () {
+                    tts.speak(tts_text);
+                  },
+                  child: Text('Speak'))
             ],
           ),
         ),
